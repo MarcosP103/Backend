b513a6b31136223956ee0e8a0fefa69984ede6f4
Marcos Perez <mperezro103@gmail.com>
2024-04-09 19:56:08 -0300

    agregar el metodo delProduct, correcciones

--- entregables/segundoentregable/app.js
+++ entregables/segundoentregable/app.js
@@ -10,97 +10,125 @@ class ProductManager {
   async uploadProducts() {
     try {
       const data = await fs.readFile(this.productsFile, "utf8");
-      this.products = JSON.parse(data)
+      this.products = JSON.parse(data);
     } catch (error) {
       if (error.code === "ENOENT") {
-        this.products = []
+        this.products = [];
       } else {
         throw error;
       }
     }
   }
 
-  async addProduct (title, description, price, thumbnail, code, stock){
-    if (!title && !description && !price && !thumbnail && !code && !stock){
-        console.log("Debe ingresar todos los campos")
-        return
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    if (!title && !description && !price && !thumbnail && !code && !stock) {
+      console.log("Debe ingresar todos los campos");
+      return;
     }
-    if(this.products.find((product) => product.code === code)){
-        console.log("El producto ya existe")
-        return
+    if (this.products.find((product) => product.code === code)) {
+      console.log("El producto ya existe");
+      return;
     }
 
-    const id = this.products.length > 0 ? Math.max(...this.products.map((p) => p.id)) + 1 : 1
+    const id =
+      this.products.length > 0
+        ? Math.max(...this.products.map((p) => p.id)) + 1
+        : 1;
     const product = {
-        id,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock
-    }
-    this.products.push(product)
-    this.addFile()
-    console.log("Producto agregado correctamente")
+      id,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+    this.products.push(product);
+    this.addFile();
+    console.log("Producto agregado correctamente");
   }
   //consult
   async consProduct() {
-    try{
-        await this.uploadProducts()
-        return this.products
-    } catch(error){
-        console.log("Error al consultar producto", error)
-        return []
+    try {
+      await this.uploadProducts();
+      return this.products;
+    } catch (error) {
+      console.log("Error al consultar producto", error);
+      return [];
     }
   }
 
-  getProducts(){
-    return this.products
+  getProducts() {
+    return this.products;
   }
 
-  getProductsById(id){
-    const product =  this.products.find((prod) => prod.id === id)
-    if(!product){
-        console.log("No se encontró el producto segun el ID indicado")
-    } return product
+  getProductsById(id) {
+    const product = this.products.find((prod) => prod.id === id);
+    if (!product) {
+      console.log("No se encontró el producto segun el ID indicado");
+    }
+    return product;
   }
 
   //mod
   async modProduct(id, productMod) {
-    const indexProd = this.products.findIndex((product) => product.id === id)
-    if(indexProd === -1){
-        console.error("No se encontro el producto por ID")
-        return
+    const indexProd = this.products.findIndex((product) => product.id === id);
+    if (indexProd === -1) {
+      console.error("No se encontro el producto por ID");
+      return;
     }
-    try{
-        this.products[indexProd] = {...this.products[indexProd], ...productMod}
-        await this.addFile()
-        console.log("Se ha agregado correctamente")
-    } catch(error){
-        console.log("Hubo un problema al actualizar", error)
+    try {
+      this.products[indexProd] = { ...this.products[indexProd], ...productMod };
+      await this.addFile();
+      console.log("Se ha agregado correctamente");
+    } catch (error) {
+      console.log("Hubo un problema al actualizar", error);
     }
   }
 
-  async addFile(){
-    try{
-        await fs.writeFile(this.productsFile, JSON.stringify(this.products, null, 2))
-        console.log("Productos agregados correctamente al archivo: ", this.productsFile)
-    } catch(error){
-        console.log("Error", error)
+  async addFile() {
+    try {
+      await fs.writeFile(
+        this.productsFile,
+        JSON.stringify(this.products, null, 2)
+      );
+      console.log(
+        "Productos agregados correctamente al archivo: ",
+        this.productsFile
+      );
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+
+  async delProduct(id) {
+    const indexProd = this.products.findIndex((product) => product.id === id);
+    if (indexProd === -1) {
+      console.error("No se encontró el producto por ID");
+      return;
+    }
+    try {
+      this.products.splice(indexProd, 1);
+      await this.addFile();
+      console.log("Producto eliminado correctamente");
+    } catch (error) {
+      console.log("Hubo un problema al eliminar", error);
     }
   }
 }
-const productManager = new ProductManager('entregables/segundoentregable/DB.json')
 
-productManager.addProduct('Remera', '100% algodon', 100, 'remera.jpg', 1, 100)
+const productManager = new ProductManager("./DB.json");
 
-const allProducts = productManager.consProduct()
-console.log("Todos los productos: ", allProducts)
+productManager.addProduct("Remera", "100% algodon", 100, "remera.jpg", 1, 100);
 
-const productById = productManager.getProductsById(1)
-console.log("el producto con ese Id: ", productById)
+const allProducts = productManager.consProduct();
+console.log("Todos los productos: ", allProducts);
 
-productManager.modProduct(1, {price: 120})
+const productById = productManager.getProductsById(1);
+console.log("el producto con ese Id: ", productById);
 
-productManager.addFile()
+productManager.modProduct(1, { price: 120 });
+
+productManager.addFile();
+
+productManager.delProduct(1);
