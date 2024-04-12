0770202117418b8d865188a19b262a77a199c89c
Marcos Perez <mperezro103@gmail.com>
2024-04-12 20:31:18 -0300

    se incluye module export

--- entregables/segundoentregable/app.js
+++ entregables/segundoentregable/app.js
@@ -113,18 +113,4 @@ class ProductManager {
   }
 }
 
-const productManager = new ProductManager("./DB.json");
-
-productManager.addProduct("Remera", "100% algodon", 100, "remera.jpg", 1, 100);
-
-const allProducts = productManager.getProducts();
-console.log("Todos los productos: ", allProducts);
-
-const productById = productManager.getProductsById(1);
-console.log("el producto con ese Id: ", productById);
-
-productManager.modProduct(1, { price: 120 });
-
-productManager.addFile();
-
-productManager.delProduct(1);
+module.exports = ProductManager
